dd3b5020d1d6cf8e6ee7312086d4f762cb873449
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2015-03-11 17:14:51 +0100

    Initial Commit

---
+++ OpenRadarBrowser/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  OpenRadarBrowser
+//
+//  Created by Nicolas Bouilleaud on 11/03/2015.
+//  Copyright (c) 2015 Nicolas Bouilleaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ OpenRadarBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenRadarBrowser
+//
+//  Created by Nicolas Bouilleaud on 11/03/2015.
+//  Copyright (c) 2015 Nicolas Bouilleaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
